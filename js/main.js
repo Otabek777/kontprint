edefcb056e83ff85a47a2d6dda08050cb500fdeb
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-03-11 23:08:15 +0500

    finish

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ $('.banner__slider').slick({
     arrows: true,
     dots: true,
 });
+const li = document.querySelectorAll('.header__navbar .li');
+
+document.querySelector('.header__burgir').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.header__navbar').classList.toggle('active');
+    document.querySelector('.bg').classList.toggle('active');
+    for(let i = 0; i < li.length; i++) {
+        li[i].classList.remove('open');
+    };
+});
+
+for(let i = 0; i < li.length; i++) {
+    li[i].addEventListener('click', function() {
+        this.classList.toggle('open');
+    });
+};
